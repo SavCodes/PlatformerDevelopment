1a5021cde2b5f8cae255f37a9e441e4018185265
SavCodes <91900522+SavCodes@users.noreply.github.com>
2024-12-10 19:00:03 -0500

    refactored position, velocity, and acceleration into 2D vectors

--- physics.py
+++ physics.py
@@ -1,24 +1,24 @@
 
 def gravity(sprite, gravity_value=0.5):
     if sprite.is_touching_ground:
-        sprite.y_acceleration = 0
-        sprite.y_velocity = 0
+        sprite.acceleration[1] = 0
+        sprite.velocity[1] = 0
 
-    elif not sprite.is_touching_ground and sprite.y_velocity < 0:
-        sprite.y_acceleration = gravity_value
+    elif not sprite.is_touching_ground and sprite.velocity[1] < 0:
+        sprite.acceleration[1] = gravity_value
 
     else:
-        sprite.y_acceleration = gravity_value * 3
+        sprite.acceleration[1] = gravity_value * 3
 
 def friction(sprite, friction_value=0.05):
-    if sprite.x_velocity != 0:
-        sprite.x_velocity *= (1 - friction_value)
+    if sprite.velocity[0] != 0:
+        sprite.velocity[0] *= (1 - friction_value)
 
 def update_physics(sprite):
     # Update the position based on velocity
-    sprite.x_position += sprite.x_velocity
-    sprite.y_position += sprite.y_velocity
+    sprite.x_position += sprite.velocity[0]
+    sprite.y_position += sprite.velocity[1]
 
     # Update the velocity based on acceleration
-    sprite.x_velocity += sprite.x_acceleration
-    sprite.y_velocity += sprite.y_acceleration
+    sprite.velocity[0] += sprite.acceleration[0]
+    sprite.velocity[1] += sprite.acceleration[1]
